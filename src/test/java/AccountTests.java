0e8fbbfd702f6f31c96c1d7a252af9b502f1aede
Maksim Zharinov <minimum.cold1991@gmail.com>
2023-08-08 00:33:18 +0700

    create tests class AccountTests.java

---
+++ src/test/java/AccountTests.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+public class AccountTests {
+
+    @Test
+    public void testGetBalance() {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    public void testTransfer() {
+        //given
+
+        //when
+
+        //then
+
+    }
+}
