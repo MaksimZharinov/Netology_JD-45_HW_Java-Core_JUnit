aeaf718ad9c4de751ca7cce8365fa86aa8eddbc7
Maksim Zharinov <minimum.cold1991@gmail.com>
2023-08-08 00:07:33 +0700

    create tests class CreditAccountTests.java

---
+++ src/test/java/CreditAccountTests.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+public class CreditAccountTests {
+
+    @Test
+    public void testAdd() {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    public void testPay() {
+        //given
+
+        //when
+
+        //then
+
+    }
+}
